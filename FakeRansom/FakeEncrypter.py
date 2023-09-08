b65281d06ceb2f6a843502802981d0029c1cec57
J/ <55988027+jkerai1@users.noreply.github.com>
2023-09-08 09:38:04 +0000

    Create FakeEncrypter.py
    Python Fake Encrypter

---
+++ FakeRansom/FakeEncrypter.py
@@ -0,0 +1,20 @@
+import os
+import time
+
+desktop = os.path.join(os.path.join(os.path.expanduser('~')), 'Desktop') #Find desktop of logged on user
+desktop = desktop + "\encrypted" ##Test path or create it
+
+if not os.path.exists(desktop):
+    os.makedirs(desktop)
+
+text = "RIP BOZO PACKWATCH" # Input the names here for the encrypted files
+FileArray = text.split(' ') ##Split to array
+
+for i in FileArray:
+    index = str(FileArray.index(i)) + ") " + i
+    FileArray[FileArray.index(i)] = index
+
+for file in FileArray:
+    temp = desktop + "\\" + file + ".encrypted" # Build final path
+    f = open(temp,"w")
+    f.close()
